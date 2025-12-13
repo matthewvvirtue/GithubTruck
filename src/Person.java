5c66031c9cc7d7b600f2d08e7912eba4c881e07d
matthewvvirtue <matthewvvirtue@gmail.com>
2025-12-13 15:32:53 -0800

    Update Person class documentation and comments

--- src/Person.java
+++ src/Person.java
@@ -1,29 +1,42 @@
 /**
- * Represents the driver of a truck.
+ * Represents a person who can act as the driver of a truck.
  */
 public class Person {
 
+    /** The name of the person */
     private String name;
 
     /**
-     * Constructs a new Person object.
+     * Constructs a new Person with a given name.
      *
-     * @param name The person's name.
+     * @param name the name of the person
      */
     public Person(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the person's name.
+     *
+     * @return the name of the person
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the person's name.
+     *
+     * @param name the new name of the person
+     */
     public void setName(String name) {
         this.name = name;
     }
 
     /**
      * Returns a string representation of the person.
+     *
+     * @return the person's name
      */
     @Override
     public String toString() {
